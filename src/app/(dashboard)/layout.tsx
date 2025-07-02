bd35f6e974bc503b7fc0ba613a5f001af4e70080
rootwitstudio <84073011+rootwitstudio@users.noreply.github.com>
2025-07-02 23:17:54 +0530

    Fix JSX syntax error in dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -12,15 +12,12 @@ export default function DashboardLayout({
       {/* Header */}
       <Header />
 
-     
-
-        {/* Main Content */}
-        <main className="flex-1 p-6">
-          <div className="max-w-7xl mx-auto">
-            {children}
-          </div>
-        </main>
-      </div>
+      {/* Main Content */}
+      <main className="flex-1 p-6">
+        <div className="max-w-7xl mx-auto">
+          {children}
+        </div>
+      </main>
     </div>
   );
 }
